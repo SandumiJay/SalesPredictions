8c9c8ca5b1c968873afb11d8abd842e2f0dca416
SandumiJay <sandumijayasekara@gmail.com>
2022-04-17 22:48:41 +0530

    Missing Values Replaced!!!!!!!!!!!!!

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -9,5 +9,26 @@ import numpy as np
 import matplotlib as plt
 import pandas as pd
 
+from sklearn.impute import SimpleImputer
+
 data = pd.read_csv('Dummy Data HSS.csv')
 print(data)
+
+X = data.iloc[:,:-1]
+print(X)
+
+Y = data.iloc[:,-1]
+print(Y)
+
+
+imputer = SimpleImputer(missing_values= np.nan, strategy ='mean')
+imputer.fit(X.iloc[:,0:3])
+X.iloc[:,0:3]=imputer.transform(X.iloc[:,0:3])
+
+
+imputer2 = SimpleImputer(missing_values= np.nan, strategy ='most_frequent')
+imputer2.fit(X.iloc[:,3:])
+X.iloc[:,3:]=imputer2.transform(X.iloc[:,3:])
+
+print("Missing Values replaced !!!!!!!")
+print(X.iloc[:,3])
