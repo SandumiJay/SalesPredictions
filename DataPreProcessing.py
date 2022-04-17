9465f1ad8e31b8dc84e81a74afeee61b5d653bc4
SandumiJay <sandumijayasekara@gmail.com>
2022-04-17 22:25:58 +0530

    Initial Commit

---
+++ DataPreProcessing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 22:20:02 2022
+
+@author: User
+"""
+
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+data = pd.read_csv('Dummy Data HSS.csv')
+print(data)
